4b27a427f46b7dab4a6e9be8fcced31678119745
Jonathan Sixt <jo211six@htwg-konstanz.de>
2026-01-21 21:41:53 +0100

    Klausur Code ergänzt

--- Klausur/EigeneArrayListe.java
+++ Klausur/EigeneArrayListe.java
@@ -12,7 +12,7 @@ public class EigeneArrayListe <T> {
         this.size = 0;
     }
 
-    public T getItem(int index) {
+    public T get(int index) {
 
         // 1. Sicherheitscheck: Ist der Index überhaupt gültig?
         // Wir dürfen nicht unter 0 gehen und nicht über size (die Anzahl der echten Elemente).
@@ -47,4 +47,60 @@ public class EigeneArrayListe <T> {
         size++;
     }
 
+    public void insert(int index, T item) {
+        // 1. Sicherheitscheck:
+        // WICHTIG: Hier ist "index == size" ERLAUBT!
+        // Das bedeutet nämlich einfach: "Füge am Ende an".
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        // 2. Kapazität prüfen (identisch zur normalen add-Methode)
+        if (size == array.length) {
+            T[] newArray = (T[]) new Object[array.length * 2];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+
+        // 3. Platz schaffen (Shifting nach RECHTS)
+        // Wir müssen RÜCKWÄRTS laufen, sonst überschreiben wir unsere eigenen Daten!
+        // Wir starten am neuen freien Ende (size) und ziehen den Vorgänger (i-1) rüber.
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+
+        // 4. Einfügen
+        array[index] = item;
+        size++;
+    }
+
+    public T remove(int index) {
+        // 1. Sicherheitscheck: Ist der Index gültig?
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        T removedItem = this.array[index]; //speichern zu entfernenen Items
+
+        // 2. Das Verschieben (Shifting)
+        // Wir starten bei dem Index, der gelöscht werden soll.
+        // Wir überschreiben ihn mit seinem rechten Nachbarn.
+        // Das wiederholen wir bis zum vorletzten Element.
+        for (int i = index + 1; 1 < this.size; i++) {
+            this.array[i-1] = this.array[i];
+        }
+
+        // 3. Aufräumen (Memory Leak vermeiden)
+        // Jetzt steht das letzte Element technisch gesehen doppelt im Array
+        // (einmal an der neuen Position und einmal noch an der alten letzten Stelle).
+        // Wir setzen die alte letzte Stelle auf null.
+        this.array[this.size - 1] = null;
+
+        // 4. Größe verringern
+        size--;
+
+        return removedItem;
+    }
 }
